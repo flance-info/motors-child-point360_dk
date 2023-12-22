98f1222dde8280473c57bdc8c71b164813ec597b
Rusty <proga100@gmail.com>
2023-12-22 11:39:28 +0500

    ss

--- inc/motors-elementor-widgets/inc/Helpers/TemplateManagerChild.php
+++ inc/motors-elementor-widgets/inc/Helpers/TemplateManagerChild.php
@@ -11,12 +11,12 @@ class TemplateManagerChild extends TemplateManager {
 	private static $single = 'Listing Template';
 	private static $setting_name = 'single_listing_template_van';
 	private static $data_for_select;
-
+	public static $selected_template_id;
 	public static function init() {
 
 		self::motors_get_templates_list();
 
-		add_action( 'init', array( self::class, 'motors_register_post_type' ) );
+		add_action( 'init', array( self::class, 'motors_register_post_type_van' ) );
 		add_action( 'wp_enqueue_scripts', array( self::class, 'remove_mew_button_component_script' ), 999 );
 		add_filter( 'me_van_car_settings_conf', array( self::class, 'motors_car_settings_conf' ) );
 		add_filter( 'wpcfto_field_mew-repeater-radio-van', array( self::class, 'motors_register_wpcfto_repeater_radio' ) );
@@ -28,7 +28,15 @@ class TemplateManagerChild extends TemplateManager {
 		wp_deregister_script( 'mew-button-component' );
 	}
 
+	public static function motors_register_post_type_van() {
 
+		self::$selected_template_id = stm_me_get_wpcfto_mod( self::$setting_name, null );
+
+		if ( null === self::$selected_template_id ) {
+			self::$selected_template_id = array_key_first( self::$data_for_select );
+		}
+
+	}
 	public static function motors_register_wpcfto_repeater_radio() {
 		return MOTORS_CHILD_ELEMENTOR_WIDGETS_PATH . '/inc/wpcfto/mew-repeater-radio-van.php';
 	}
@@ -62,6 +70,20 @@ class TemplateManagerChild extends TemplateManager {
 			);
 		}
 		self::$data_for_select = $for_select;
+		wp_reset_postdata();
+	}
+
+	public static function motors_display_template_van() {
+		global $post;
+		$special_listing_template = get_post_meta( $post->ID, 'special_listing_template', true );
+		flance_write_log($special_listing_template, 'logs/special_listing_template.log');
+		$template_listing_id      = ( $special_listing_template ) ? $special_listing_template : self::$selected_template_id;
+
+		flance_write_log($template_listing_id, 'logs/template_listing_id_logs.log');
+		$template_listing         = get_post( $template_listing_id );
+		setup_postdata( $template_listing );
+		//phpcs:ignore
+		echo Plugin::instance()->frontend->get_builder_content_for_display( $template_listing->ID );
 		wp_reset_postdata();
 	}
 
--- inc/motors-elementor-widgets/inc/Helpers/TemplateManagerLeasingChild.php
+++ inc/motors-elementor-widgets/inc/Helpers/TemplateManagerLeasingChild.php
@@ -12,12 +12,13 @@ class TemplateManagerLeasingChild extends TemplateManager {
 	private static $single = 'Listing Template';
 	private static $setting_name = 'single_listing_template_leasing';
 	private static $data_for_select;
+	public static $selected_template_id;
 
 	public static function init() {
 
 		self::motors_get_templates_list();
 
-		add_action( 'init', array( self::class, 'motors_register_post_type' ) );
+		add_action( 'init', array( self::class, 'motors_register_post_type_leasing' ) );
 		add_action( 'wp_enqueue_scripts', array( self::class, 'remove_mew_button_component_script' ), 999 );
 		add_filter( 'me_leasing_car_settings_conf', array( self::class, 'motors_car_settings_conf' ) );
 		add_filter( 'wpcfto_field_mew-repeater-radio-leasing', array( self::class, 'motors_register_wpcfto_repeater_radio' ) );
@@ -29,6 +30,15 @@ class TemplateManagerLeasingChild extends TemplateManager {
 		wp_deregister_script( 'mew-button-component' );
 	}
 
+	public static function motors_register_post_type_leasing() {
+
+		self::$selected_template_id = stm_me_get_wpcfto_mod( self::$setting_name, null );
+
+		if ( null === self::$selected_template_id ) {
+			self::$selected_template_id = array_key_first( self::$data_for_select );
+		}
+
+	}
 
 	public static function motors_register_wpcfto_repeater_radio() {
 		return MOTORS_CHILD_ELEMENTOR_WIDGETS_PATH . '/inc/wpcfto/mew-repeater-radio-leasing.php';
@@ -63,6 +73,20 @@ class TemplateManagerLeasingChild extends TemplateManager {
 			);
 		}
 		self::$data_for_select = $for_select;
+		wp_reset_postdata();
+	}
+
+	public static function motors_display_template_leasing() {
+		global $post;
+		$special_listing_template = get_post_meta( $post->ID, 'special_listing_template', true );
+		flance_write_log($special_listing_template, 'logs/special_listing_template.log');
+		$template_listing_id      = ( $special_listing_template ) ? $special_listing_template : self::$selected_template_id;
+
+		flance_write_log($template_listing_id, 'logs/template_listing_id_logs.log');
+		$template_listing         = get_post( $template_listing_id );
+		setup_postdata( $template_listing );
+		//phpcs:ignore
+		echo Plugin::instance()->frontend->get_builder_content_for_display( $template_listing->ID );
 		wp_reset_postdata();
 	}
 }
