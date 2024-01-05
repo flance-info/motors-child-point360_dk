f1896a08df920dd464f714c835e3f57eae9e439a
Rusty <proga100@gmail.com>
2024-01-05 14:20:11 +0500

    aa

--- inc/loader.php
+++ inc/loader.php
@@ -13,15 +13,15 @@ function flance_write_log( $message, $file = 'logs/logfile.log' ) {
 	file_put_contents( $log_file_path, ' ' . $message . "\n",  LOCK_EX );
 }
 
+function enqueue_custom_scripts() {
+	wp_enqueue_style('general-style', get_stylesheet_directory_uri() . '/assets/css/new-style.css', [], time());
+    wp_enqueue_script('stm-butterbean_child', get_stylesheet_directory_uri() . "/assets/js/stm_butterbean_fields_child.js", array(), time(), true);
+    wp_enqueue_script('stm-theme-multiselect', STM_LISTINGS_URL . '/assets/js/jquery.multi-select.js', array('jquery'), time(), true);
+}
+add_action('wp_enqueue_scripts', 'enqueue_custom_scripts');
 
 require_once 'motors-elementor-widgets/loader.php';
 require_once 'stm-motors-extends/loader.php';
 require_once 'vehicle_functions/vehicle_functions.php';
-require_once(STM_LISTINGS_PATH . '/includes/admin/butterbean/butterbean.php');
-wp_enqueue_style( 'general-style', get_stylesheet_directory_uri(). '/assets/css/new-style.css', [], time() );
 
-wp_enqueue_script('stm-butterbean', STM_LISTINGS_URL . "/includes/admin/butterbean/js/stm_butterbean_fields_child.js", array(), time(), true);
-
-wp_enqueue_script( 'stm-theme-multiselect', STM_LISTINGS_URL . '/assets/js/jquery.multi-select.js', array( 'jquery' ) );
-wp_enqueue_script( 'stm-listings-js', STM_LISTINGS_URL . '/assets/js/vehicles-listing.js', array( 'jquery', 'jquery-ui-droppable', 'jquery-ui-datepicker', 'jquery-ui-sortable' ), '6.5.8.1' );
 
