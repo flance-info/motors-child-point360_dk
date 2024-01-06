0978495a0b229ea63d1d854ccd4d21d041f88087
Rusty <proga100@gmail.com>
2024-01-06 23:55:08 +0500

    Update stm-elementor-buttons.php

--- templates/widgets/stm-buttons/stm-elementor-buttons.php
+++ templates/widgets/stm-buttons/stm-elementor-buttons.php
@@ -2,8 +2,9 @@
 
 $current_url  = home_url( $_SERVER['REQUEST_URI'] );
 $layout_param = filter_input( INPUT_GET, 'stm-layout', FILTER_SANITIZE_STRING );
-
-
+$post_id      = get_the_ID();
+$add_leasing  = get_post_meta( $post_id, 'stm_add_leasing', true );
+$add_van      = get_post_meta( $post_id, 'stm_add_van', true );
 ?>
 
 <div class="elementor-row stm-buttons">
@@ -13,32 +14,42 @@ $layout_param = filter_input( INPUT_GET, 'stm-layout', FILTER_SANITIZE_STRING );
 				<span class="elementor-button-text"><?php esc_html_e( 'SHOW AS CAR', 'motors-child' ); ?></span>
 			</a>
 		</div>
-		<div class="elementor-column">
-			<a href="?stm-layout=van" class="elementor-button elementor-size-md black-button">
-				<span class="elementor-button-text"><?php esc_html_e( 'SHOW AS VAN', 'motors-child' ); ?></span>
-			</a>
-		</div>
-		<div class="elementor-column">
-			<a href="?stm-layout=leasing_van" class="elementor-button elementor-size-md black-button">
-				<span class="elementor-button-text"><?php esc_html_e( 'SHOW AS LEASING VAN', 'motors-child' ); ?></span>
-			</a>
-		</div>
+		<?php if ( $add_van == 'yes' ) : ?>
+			<div class="elementor-column">
+				<a href="?stm-layout=van" class="elementor-button elementor-size-md black-button">
+					<span class="elementor-button-text"><?php esc_html_e( 'SHOW AS VAN', 'motors-child' ); ?></span>
+				</a>
+			</div>
+		<?php endif; ?>
+
+		<?php if ( $add_leasing == 'yes' ) : ?>
+			<div class="elementor-column">
+				<a href="?stm-layout=leasing_van" class="elementor-button elementor-size-md black-button">
+					<span class="elementor-button-text"><?php esc_html_e( 'SHOW AS LEASING VAN', 'motors-child' ); ?></span>
+				</a>
+			</div>
+		<?php endif; ?>
 	<?php elseif ( 'van' == $layout_param ) : ?>
 		<div class="elementor-column">
 			<a href="?" class="elementor-button elementor-size-md black-button">
 				<span class="elementor-button-text"><?php esc_html_e( 'SHOW AS CAR', 'motors-child' ); ?></span>
 			</a>
 		</div>
-		<div class="elementor-column">
-			<a href="?stm-layout=leasing" class="elementor-button elementor-size-md black-button">
-				<span class="elementor-button-text"><?php esc_html_e( 'SHOW AS LEASING CAR', 'motors-child' ); ?></span>
-			</a>
-		</div>
-		<div class="elementor-column">
-			<a href="?stm-layout=leasing_van" class="elementor-button elementor-size-md black-button">
-				<span class="elementor-button-text"><?php esc_html_e( 'SHOW AS LEASING VAN', 'motors-child' ); ?></span>
-			</a>
-		</div>
+
+		<?php if ( $add_leasing == 'yes' ) : ?>
+			<div class="elementor-column">
+				<a href="?stm-layout=leasing" class="elementor-button elementor-size-md black-button">
+					<span class="elementor-button-text"><?php esc_html_e( 'SHOW AS LEASING CAR', 'motors-child' ); ?></span>
+				</a>
+			</div>
+		<?php endif; ?>
+		<?php if ( $add_leasing == 'yes' ) : ?>
+			<div class="elementor-column">
+				<a href="?stm-layout=leasing_van" class="elementor-button elementor-size-md black-button">
+					<span class="elementor-button-text"><?php esc_html_e( 'SHOW AS LEASING VAN', 'motors-child' ); ?></span>
+				</a>
+			</div>
+		<?php endif; ?>
 	<?php elseif ( 'leasing_van' == $layout_param ) : ?>
 
 		<div class="elementor-column">
@@ -51,27 +62,33 @@ $layout_param = filter_input( INPUT_GET, 'stm-layout', FILTER_SANITIZE_STRING );
 				<span class="elementor-button-text"><?php esc_html_e( 'SHOW AS VAN', 'motors-child' ); ?></span>
 			</a>
 		</div>
-		<div class="elementor-column">
-			<a href="?stm-layout=leasing" class="elementor-button elementor-size-md black-button">
-				<span class="elementor-button-text"><?php esc_html_e( 'SHOW AS LEASING CAR', 'motors-child' ); ?></span>
-			</a>
-		</div>
+		<?php if ( $add_leasing == 'yes' ) : ?>
+			<div class="elementor-column">
+				<a href="?stm-layout=leasing" class="elementor-button elementor-size-md black-button">
+					<span class="elementor-button-text"><?php esc_html_e( 'SHOW AS LEASING CAR', 'motors-child' ); ?></span>
+				</a>
+			</div>
+		<?php endif; ?>
 	<?php else : ?>
 		<div class="elementor-column">
 			<a href="?stm-layout=van" class="elementor-button elementor-size-md black-button">
 				<span class="elementor-button-text"><?php esc_html_e( 'SHOW AS VAN', 'motors-child' ); ?></span>
 			</a>
 		</div>
-		<div class="elementor-column">
-			<a href="?stm-layout=leasing" class="elementor-button elementor-size-md black-button">
-				<span class="elementor-button-text"><?php esc_html_e( 'SHOW AS LEASING CAR', 'motors-child' ); ?></span>
-			</a>
-		</div>
-		<div class="elementor-column">
-			<a href="?stm-layout=leasing_van" class="elementor-button elementor-size-md black-button">
-				<span class="elementor-button-text"><?php esc_html_e( 'SHOW AS LEASING VAN', 'motors-child' ); ?></span>
-			</a>
-		</div>
+		<?php if ( $add_leasing == 'yes' ) : ?>
+			<div class="elementor-column">
+				<a href="?stm-layout=leasing" class="elementor-button elementor-size-md black-button">
+					<span class="elementor-button-text"><?php esc_html_e( 'SHOW AS LEASING CAR', 'motors-child' ); ?></span>
+				</a>
+			</div>
+		<?php endif; ?>
+		<?php if ( $add_leasing == 'yes' ) : ?>
+			<div class="elementor-column">
+				<a href="?stm-layout=leasing_van" class="elementor-button elementor-size-md black-button">
+					<span class="elementor-button-text"><?php esc_html_e( 'SHOW AS LEASING VAN', 'motors-child' ); ?></span>
+				</a>
+			</div>
+		<?php endif; ?>
 	<?php endif; ?>
 </div>
 
