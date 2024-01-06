8827546a37dd27fba114c69f3e7181d13d9a2d6a
Rusty <proga100@gmail.com>
2024-01-06 22:41:48 +0500

    stm buttons

---
+++ inc/motors-elementor-widgets/inc/widgets/class-stm-elementor-buttons.php
@@ -0,0 +1,97 @@
+<?php
+
+use Elementor\Widget_Base;
+use Elementor\Controls_Manager;
+use Elementor\Group_Control_Box_Shadow;
+use Elementor\Group_Control_Typography;
+use Elementor\Group_Control_Text_Shadow;
+use Elementor\Group_Control_Border;
+use Elementor\Group_Control_Background;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+class Stm_Elementor_Buttons extends Widget_Base {
+	protected $handler = 'stm-elementor-buttons';
+
+	public function get_name() {
+		return 'stm-elementor-buttons';
+	}
+
+	public function get_title() {
+		return esc_html__( 'Motors Leasing, Van, Car buttons', 'motors-child' );
+	}
+
+	public function get_icon() {
+		return 'eicon-woo-cart';
+	}
+
+	public function get_keywords() {
+		return [ 'buttons', 'stylemix', 'motors' ];
+	}
+
+	public function get_categories() {
+		return [ 'motors' ];
+	}
+
+	public function get_script_depends() {
+		return array( $this->handler );
+	}
+
+	public function get_style_depends() {
+		return array( $this->handler );
+	}
+
+
+	protected function register_controls() {
+		$this->start_controls_section(
+			'section_content',
+			[
+				'label' => esc_html__( 'General', 'elementor-pro' ),
+			]
+		);
+		$templates = $this->get_elementor_templates(); // Function to fetch Elementor templates and their IDs
+		$this->add_control(
+			'additional_template_select',
+			[
+				'label'              => esc_html__( 'Select Template', 'motors-child' ),
+				'type'               => Controls_Manager::SELECT2,
+				'options'            => $templates,
+				'label_block'        => true,
+				'show_label'         => false,
+				'frontend_available' => true,
+				'render_type'        => 'template',
+			]
+		);
+		$this->end_controls_section();
+
+	}
+
+	private function get_elementor_templates() {
+		$templates = [];
+		// Check if Elementor is active
+		if ( class_exists( 'Elementor\Plugin' ) ) {
+			$templates_manager = \Elementor\Plugin::$instance->templates_manager;
+			// Get all templates
+			$templates_data = $templates_manager->get_source( 'local' )->get_items();
+			foreach ( $templates_data as $template ) {
+				$templates[ $template['template_id'] ] = $template['title'];
+			}
+		}
+
+		return $templates;
+	}
+
+	public function render() {
+
+		// Display our Widget.
+		$template_id = intval( $this->get_settings_for_display( 'additional_template_select' ) );
+		if ( ! empty( $template_id ) ) {
+			echo do_shortcode( '[elementor-template id="' . $template_id . '"]' );
+		}
+
+	}
+
+}
+
--- inc/motors-elementor-widgets/loader.php
+++ inc/motors-elementor-widgets/loader.php
@@ -4,3 +4,12 @@ define ('MOTORS_CHILD_ELEMENTOR_WIDGETS_URL', get_stylesheet_directory_uri().'/i
 
 require_once 'inc/Helpers/TemplateManagerChild.php';
 require_once 'inc/Helpers/TemplateManagerLeasingChild.php';
+
+add_action('init', 'stm_add_init_widgets');
+function stm_add_init_widgets() {
+	add_action( 'elementor/widgets/register', 'stm_add_widgets' );
+}
+function stm_add_widgets() {
+	require_once 'inc/widgets/class-stm-elementor-buttons.php';
+	\Elementor\Plugin::instance()->widgets_manager->register( new Stm_Elementor_Buttons() );
+}
