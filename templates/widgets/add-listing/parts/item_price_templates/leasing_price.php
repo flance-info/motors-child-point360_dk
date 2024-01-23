ae76c191ddd27ea57ed05a281520b5594ae9bd54
Rusty <proga100@gmail.com>
2024-01-23 11:12:41 +0500

    Update leasing_price.php

--- templates/widgets/add-listing/parts/item_price_templates/leasing_price.php
+++ templates/widgets/add-listing/parts/item_price_templates/leasing_price.php
@@ -1,6 +1,6 @@
 <div class="col-md-4 col-sm-6">
 	<div class="stm_price_input">
-		<div class="stm_label heading-font"><?php esc_html_e( 'Leasing Car Price', 'motors-child' ); ?>*
+		<div class="stm_label heading-font"><?php esc_html_e( 'Bil: Leasingydelse pr. mdr.', 'motors-child' ); ?>
 			(<?php echo wp_kses_post( stm_get_price_currency() ); ?>)
 		</div>
 		<input type="number" class="heading-font" name="stm_leasing_car_price" value="<?php echo esc_attr( $stm_leasing_car_price ); ?>" required/>
@@ -8,7 +8,7 @@
 </div>
 <div class="col-md-4 col-sm-6">
 	<div class="stm_price_input">
-		<div class="stm_label heading-font"><?php esc_html_e( 'Leasing Van Price', 'motors-child' ); ?>*
+		<div class="stm_label heading-font"><?php esc_html_e( 'Van: Leasingydelse pr. mdr.', 'motors-child' ); ?>
 			(<?php echo wp_kses_post( stm_get_price_currency() ); ?>)
 		</div>
 		<input type="number" class="heading-font" name="stm_leasing_van_price" value="<?php echo esc_attr( $stm_leasing_van_price ); ?>" required/>
